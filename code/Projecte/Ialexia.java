a4ad34c76e98782261736bd9c5b47106a9f7d160
Jhon <jmoreno1@iesebre.com>
2021-11-18 08:29:50 +0100

    WIP

---
+++ code/Projecte/Ialexia.java
@@ -0,0 +1,11 @@
+package UF1.Projecte;
+
+public class Ialexia {
+
+    // Constructor
+    void Person (){}
+    boolean talking () {
+        System.out.println("hola");
+        return true;
+    }
+}
--- code/Projecte/Simulador.java
+++ code/Projecte/Simulador.java
@@ -14,6 +14,7 @@ public class Simulador {
                 System.out.println(" - Persona");
                 System.out.println(" - Animal");
                 System.out.println(" - Extraterrestre");
+                System.out.println(" - Ialexia");
                 System.out.print("Opció: ");
                 switch (scanner.nextLine()) {
                     case "Persona":
@@ -28,52 +29,12 @@ public class Simulador {
                         Alien extraterrestre = new Alien();
                         finish = extraterrestre.talking();
                         break;
+                    case "Ialexia":
+                        Ialexia ialexia = new Ialexia();
+                        finish = ialexia.talking();
+                        break;
                 }
             } while (!finish);
-
-        do {
-            Scanner scanner = new Scanner(System.in);
-            // Questionari
-            System.out.println("Qüestionari");
-            System.out.println(" 1. Quin tipus de persona ets?");
-            System.out.println(" 2. Quin esport t'agrada");
-            System.out.println(" 3. Quin és el teu número favorit entre 0 i ...?");
-            System.out.println(" 4. Sabries llistar tots els nombres parells resultants de la multiplicació entre el valor enter a i b, on el valor de a creixerà fins b");
-            System.out.println(" 5. Juguem a pedra papaer i tisora?");
-            System.out.println(" 6. Posa-li un nom!");
-            System.out.println(" 7. Pots recorda-me la nostra conversa?");
-            System.out.println(" 8. Sortir");
-            System.out.print("Opció: ");
-            switch (scanner.nextInt()) {
-                case 1:
-
-                    break;
-                case 2:
-
-                    break;
-                case 3:
-
-                    break;
-                case 4:
-
-                    break;
-                case 5:
-
-                    break;
-                case 6:
-
-                    break;
-                case 7:
-
-                    break;
-                case 8:
-
-                    break;
-            }
-        } while (!finish);
-
-
-
 
         }
     }
